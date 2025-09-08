571b1e0b4fd5959f8ff7d3210082723375af1608
br-maria <m.breewa@gmail.com>
2025-09-09 00:29:16 +0300

    Update EventsApplication.java

--- src/microservices/events/src/main/java/com/cinemaabyss/events/EventsApplication.java
+++ src/microservices/events/src/main/java/com/cinemaabyss/events/EventsApplication.java
@@ -3,9 +3,11 @@ package com.cinemaabyss.events;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 @EnableKafka
+@EnableRetry
 public class EventsApplication {
     public static void main(String[] args) {
         SpringApplication.run(EventsApplication.class, args);
